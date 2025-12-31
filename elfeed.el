307e6dbc0a1997cf7e391aef6e60cfb5d96fecc4
Florian Marrero Liestmann <f.m.liestmann@fx-ttr.de>
2025-12-31 12:06:40 +0100

    Add security rss feeds

--- elfeed.el
+++ elfeed.el
@@ -2,4 +2,19 @@
   :ensure t
   :config
   (setq elfeed-feeds
-	'("https://planet.guix.gnu.org/atom.xml")))
+	'("https://feeds.arstechnica.com/arstechnica/technology-lab"
+	  "https://bleepingcomputer.com/feed/"
+	  "https://googleonlinesecurity.blogspot.com/atom.xml"
+	  "https://googleprojectzero.blogspot.com/feeds/posts/default"
+	  "https://www.grahamcluley.com/feed/"
+	  "https://jfrog.com/blog/feed"
+	  "https://krebsonsecurity.com/feed/"
+	  "https://www.schneier.com/feed/atom/"
+	  "https://techcrunch.com/feed/"
+	  "https://feeds.feedburner.com/TroyHunt"
+	  "https://lobste.rs/top/rss"
+	  "https://risky.biz/feeds/risky-business/"
+	  "https://risky.biz/feeds/risky-business-news/"
+	  "https://www.theregister.com/security/research/headlines.atom"
+	  "https://www.theregister.com/security/patches/headlines.atom"
+	  "https://www.theregister.com/security/cyber_crime/headlines.atom")))
