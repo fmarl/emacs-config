1b42158c5f2d556132ac57a908b3c5efae7d068c
Florian Marrero Liestmann <f.m.liestmann@fx-ttr.de>
2026-01-04 23:13:07 +0100

    Add more security related blogs to elfeed

--- elfeed.el
+++ elfeed.el
@@ -2,20 +2,21 @@
   :ensure t
   :config
   (setq elfeed-feeds
-	'("https://planet.guix.gnu.org/atom.xml"
-	  "https://feeds.arstechnica.com/arstechnica/technology-lab"
+	'("https://feeds.arstechnica.com/arstechnica/technology-lab"
 	  "https://bleepingcomputer.com/feed/"
 	  "https://googleonlinesecurity.blogspot.com/atom.xml"
 	  "https://googleprojectzero.blogspot.com/feeds/posts/default"
+	  "https://bughunters.google.com/feed/en"
 	  "https://www.grahamcluley.com/feed/"
 	  "https://jfrog.com/blog/feed"
 	  "https://krebsonsecurity.com/feed/"
 	  "https://www.schneier.com/feed/atom/"
-	  "https://techcrunch.com/feed/"
 	  "https://feeds.feedburner.com/TroyHunt"
+	  "https://feeds.feedburner.com/TheHackersNews"
 	  "https://lobste.rs/top/rss"
 	  "https://risky.biz/feeds/risky-business/"
 	  "https://risky.biz/feeds/risky-business-news/"
 	  "https://www.theregister.com/security/research/headlines.atom"
 	  "https://www.theregister.com/security/patches/headlines.atom"
-	  "https://www.theregister.com/security/cyber_crime/headlines.atom")))
+	  "https://www.theregister.com/security/cyber_crime/headlines.atom"
+	  "https://www.darkreading.com/rss.xml")))
